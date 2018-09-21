b485be2290a9b8c1b549a4d448999ae71dbb4fe0
Al Davidson <apdavidson@gmail.com>
2018-09-21 10:53:55 +0100

    get & post requests now work & are fully-specced. No auth yet still.

--- app/controllers/user_data_controller.rb
+++ app/controllers/user_data_controller.rb
@@ -1,8 +1,38 @@
 class UserDataController < ApplicationController
   def show
-    @user_data = UserData.find_by!(user_identifier: params[:user_id],
-                                   service_slug: params[:service_slug])
+    @user_data = UserData.find_by!(record_retrieval_params)
 
     render json: ::UserDataPresenter.new(@user_data), status: :ok
+  end
+
+  # To keep things simple and fast on the client, we'll transparently
+  # handle create or update in one method, called via POST
+  def create_or_update
+    @user_data = UserData.where(record_retrieval_params).first
+
+    if @user_data
+      success_status = :no_content
+    else
+      success_status = :created
+      @user_data = UserData.new(record_retrieval_params)
+    end
+
+    @user_data.payload = user_data_params[:payload]
+    @user_data.save!
+
+    render status: success_status, format: :json
+  end
+
+  private
+
+  def record_retrieval_params(opts = params)
+    {
+      user_identifier: params[:user_id],
+      service_slug: params[:service_slug]
+    }
+  end
+
+  def user_data_params(opts = params)
+    opts.permit(:payload)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/service/:service_slug/user/:user_id', to: 'user_data#show'
+  post '/service/:service_slug/user/:user_id', to: 'user_data#create_or_update'
 end
