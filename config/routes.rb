637649df1369e2a87d023b12a3659af50441310c
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-03-29 08:21:07 +0000

    Add route for endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/service/:service_slug/user/:user_id', to: 'user_data#show'
   post '/service/:service_slug/user/:user_id', to: 'user_data#create_or_update'
+  post '/service/:service_slug/savereturn/email/add', to: 'emails#create'
 end
