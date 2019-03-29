36fb7309c36c9353607642c4152ef0aca99035aa
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-03-29 16:20:17 +0000

    If email record already exists, mark it as superseded and create a new record and set it to valid

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,11 +2,14 @@ class EmailsController < ApplicationController
   def create
     expires = Time.now + email_params[:duration].minutes
 
+    find_record(record_retrieval_params)
+
     email_data = Email.new(email: email_params[:email_for_sending],
                            unique_id: SecureRandom.uuid,
                            service_slug: params[:service_slug],
                            encrypted_payload: email_params[:email_details],
-                           expires_at: expires)
+                           expires_at: expires,
+                           validity: 'valid')
 
     email_data.save!
 
@@ -17,5 +20,20 @@ class EmailsController < ApplicationController
 
   def email_params
     params.permit(:email_for_sending, :email_details, :duration, :link_template)
+  end
+
+  def find_record(attributes)
+    email = Email.where(attributes).first
+    return if email.nil?
+
+    email.update_attributes!(validity: 'superseded')
+  end
+
+  def record_retrieval_params
+    {
+      email: email_params[:email_for_sending],
+      encrypted_payload: email_params[:email_details],
+      service_slug: params[:service_slug]
+    }
   end
 end
--- spec/requests/emails_spec.rb
+++ spec/requests/emails_spec.rb
@@ -30,9 +30,51 @@ RSpec.describe EmailsController, type: :request do
         }
       end
 
-      it 'returns a 201 status' do
-        post_request
-        expect(response).to have_http_status(201)
+      context 'when the email record does not exist' do
+        it 'returns a 201 status' do
+          post_request
+          expect(response).to have_http_status(201)
+        end
+
+        it 'sets validity to `valid`' do
+          post_request
+          expect(Email.first.validity).to eq('valid')
+        end
+      end
+
+      context 'when the email record already exist' do
+        let(:existing_record) do
+          Email.create!(id: SecureRandom.uuid,
+                        unique_id: '5db4f4e3-71ef-4784-a03a-2f2a490174f2',
+                        email: 'jane-doe@example.com',
+                        service_slug: service_slug,
+                        encrypted_payload: '64c0b8afa7e93d51c1fc5fe82cac4a690927ee1aa5883b985',
+                        expires_at: Time.now + 20.minutes,
+                        validity: 'valid')
+        end
+
+        before do
+          existing_record
+          post_request
+        end
+
+        it 'there are two records with the same email address' do
+          expect(Email.where(email: 'jane-doe@example.com').count).to eq(2)
+        end
+
+        it 'sets validity of existing record to `superseded`' do
+          old_record = Email.find_by_unique_id(existing_record.unique_id)
+          expect(old_record.validity).to eq('superseded')
+        end
+
+        it 'sets newest created record validity to `valid`' do
+          new_record = Email.where.not(unique_id: existing_record.unique_id).first
+          expect(new_record.validity).to eq('valid')
+        end
+
+        it 'returns a 201 status' do
+          expect(response).to have_http_status(201)
+        end
       end
     end
   end
