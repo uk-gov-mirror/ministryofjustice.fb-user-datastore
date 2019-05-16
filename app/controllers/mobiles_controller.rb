0aa09deefbaee123373e9b5dbf374d4a64cb2edf
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-05-16 21:18:26 +0100

    Create mobile record

--- app/controllers/mobiles_controller.rb
+++ app/controllers/mobiles_controller.rb
@@ -1,4 +1,13 @@
 class MobilesController < ApplicationController
   def create
+    mobile_data = Mobile.new(service_slug: params[:service_slug],
+                             mobile: params[:mobile],
+                             encrypted_email: params[:encrypted_email],
+                             encrypted_payload: params[:encrypted_details],
+                             expires_at: Time.now + (params[:duration].to_i).hours,
+                             code: '12345')
+
+    mobile_data.save!
+    render json: {}, status: :created
   end
 end
--- spec/controllers/mobiles_controller_spec.rb
+++ spec/controllers/mobiles_controller_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe MobilesController, type: :controller do
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:verify_token!)
+    request.env['CONTENT_TYPE'] = 'application/json'
+  end
 
+  let(:service_slug) { 'my-service' }
+
+  describe 'POST /service/:service_slug/savereturn/mobile/add' do
+    context 'when the mobile record does not exist' do
+      let(:post_request) do
+        post :create, params: { service_slug: service_slug,
+                                mobile: '07777 111 222',
+                                encrypted_email: 'encryptedEmail',
+                                encrypted_details: 'encryptedDetails',
+                                duration: '20' }
+      end
+
+      it 'persists the record' do
+        expect do
+          post_request
+        end.to change(Mobile, :count).by(1)
+      end
+
+      it 'returns a 201 status' do
+        post_request
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns an empty json object' do
+        post_request
+        expect(response.body).to eql('{}')
+      end
+
+      it 'sets record values correctly' do
+        post_request
+        record = Mobile.last
+
+        expect(record.mobile).to eq(request.parameters[:mobile])
+        expect(record.encrypted_email).to eq(request.parameters[:encrypted_email])
+        expect(record.service_slug).to eq('my-service')
+        expect(record.encrypted_payload).to eq(request.parameters[:encrypted_details])
+        expect(record.expires_at).to_not be_blank
+        expect(record.code).to_not be_blank
+        expect(record.validity).to eq('valid')
+      end
+    end
+  end
 end
